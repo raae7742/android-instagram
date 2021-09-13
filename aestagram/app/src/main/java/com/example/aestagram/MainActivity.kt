38abf4c01e4f3db72f7ddd9dc87bf118183304f9
Hyeonae Jang <aeae_1@naver.com>
2021-09-13 23:39:05 +0900

    Add: 메인 네비게이션 탭바

--- aestagram/app/src/main/java/com/example/aestagram/MainActivity.kt
+++ aestagram/app/src/main/java/com/example/aestagram/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.aestagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.aestagram.navigation.AlarmFragment
+import com.example.aestagram.navigation.DetailViewFragment
+import com.example.aestagram.navigation.GridFragment
+import com.example.aestagram.navigation.UserFragment
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationBarView.OnItemSelectedListener {
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridViewFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridViewFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnItemSelectedListener(this)
     }
 }
