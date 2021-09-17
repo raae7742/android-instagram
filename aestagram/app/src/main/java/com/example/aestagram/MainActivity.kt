f1dac094f290c5ceb4cc0bc5ebb2fb4d111cfe76
Hyeonae Jang <aeae_1@naver.com>
2021-09-17 16:15:56 +0900

    Add: 사진 업로드 페이지

--- aestagram/app/src/main/java/com/example/aestagram/MainActivity.kt
+++ aestagram/app/src/main/java/com/example/aestagram/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.aestagram
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.aestagram.navigation.AlarmFragment
 import com.example.aestagram.navigation.DetailViewFragment
 import com.example.aestagram.navigation.GridFragment
@@ -10,6 +15,7 @@ import com.example.aestagram.navigation.UserFragment
 import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(),NavigationBarView.OnItemSelectedListener {
 
@@ -26,6 +32,9 @@ class MainActivity : AppCompatActivity(),NavigationBarView.OnItemSelectedListene
                 return true
             }
             R.id.action_add_photo -> {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
             }
             R.id.action_favorite_alarm -> {
                 var alarmFragment = AlarmFragment()
@@ -45,5 +54,7 @@ class MainActivity : AppCompatActivity(),NavigationBarView.OnItemSelectedListene
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnItemSelectedListener(this)
+
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 }
