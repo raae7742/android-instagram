b126b5919b8d6639fad071e50460e43452905a4a
Hyeonae Jang <aeae_1@naver.com>
2021-09-18 14:43:08 +0900

    Add: 좋아요 기능

--- aestagram/app/src/main/java/com/example/aestagram/navigation/DetailViewFragment.kt
+++ aestagram/app/src/main/java/com/example/aestagram/navigation/DetailViewFragment.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Glide.init
 import com.example.aestagram.R
 import com.example.aestagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
@@ -18,11 +19,13 @@ import kotlinx.android.synthetic.main.item_detail.view.*
 class DetailViewFragment : Fragment() {
 
     var firestore : FirebaseFirestore? = null
+    var uid : String? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
 
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
 
         view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
@@ -70,13 +73,44 @@ class DetailViewFragment : Fragment() {
             //likes
             viewHolder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![position].favoriteCount
 
-            //ProfileImages
-            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewHolder.detailviewitem_profile_image)
+            // This code is when the button is clicked
+            viewHolder.detailviewitem_favorite_imageview.setOnClickListener{
+                favoriteEvent(position)
+            }
 
+            // This code is when the page is loaded
+            if (contentDTOs!![position].favorites.containsKey(uid)) {
+                //This is like status
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
+
+            } else {
+                //This is unlike status
+                viewHolder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)
+            }
+            //ProfileImages
+            //Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewHolder.detailviewitem_profile_image)
         }
 
         override fun getItemCount(): Int {
             return contentDTOs.size
+        }
+
+        fun favoriteEvent(position : Int) {
+            var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction ->
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if (contentDTO!!.favorites.containsKey(uid)){
+                    // When the button is clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+                } else {
+                    // When the button is not clicked
+                    contentDTO?.favoriteCount = contentDTO?.favoriteCount + 1
+                    contentDTO?.favorites[uid!!] = true
+                }
+                transaction.set(tsDoc, contentDTO)
+            }
         }
     }
 }
--- aestagram/app/src/main/java/com/example/aestagram/navigation/model/ContentDTO.kt
+++ aestagram/app/src/main/java/com/example/aestagram/navigation/model/ContentDTO.kt
@@ -1,12 +1,12 @@
 package com.example.aestagram.navigation.model
 
-class ContentDTO (  var explain : String? = null,
+data class ContentDTO (  var explain : String? = null,
                     var imageUrl : String? = null,
                     var uid : String? = null,
                     var userId : String? = null,
                     var timestamp : Long? = null,
                     var favoriteCount : Int = 0,
-                    var favorites : Map<String,Boolean> = HashMap()) {
+                    var favorites : MutableMap<String,Boolean> = HashMap()) {
 
     data class Comment(var uid : String? = null,
                         var userId : String? = null,
